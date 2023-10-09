97ac8d7121ef67c5e433502df165aa17ab6f3c0e
sahil <38555487+sahilgohil@users.noreply.github.com>
2023-10-10 08:37:25 +1100

    Analytics View Complete

---
+++ HealthCareManagementSystem/src/main/java/com/mycompany/healthcaremanagementsystem/ViewAnalyticsController.java
@@ -0,0 +1,37 @@
+
+package com.mycompany.healthcaremanagementsystem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.TextArea;
+/**
+ * FXML Controller class
+ *
+ * @author sahil
+ */
+public class ViewAnalyticsController implements Initializable {
+
+
+    @FXML
+    private TextArea analyticsArea;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void homeBtnEvent(ActionEvent event) {
+    }
+
+    @FXML
+    private void logOutEvent(ActionEvent event) {
+    }
+
+}
